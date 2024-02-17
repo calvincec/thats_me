d167b074caaf8d2a86423469d4190ec96800be32
Calvince Ndemo <calyndemo16@gmail.com>
2024-02-17 12:06:09 +0300

    added them all

---
+++ src/script.js
@@ -0,0 +1,4 @@
+let navbar = document.querySelector('.navbar');
+let home = document.querySelector('.home');
+let about = document.querySelector('.about');
+let projects = document.querySelector('.projects');
